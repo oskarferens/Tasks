fed4e5282e526fda6e5f0eb060f45a350faeb38a
Oskar Ferens <oskar.ferens1@gmail.com>
2021-07-13 12:18:30 +0200

    22.2

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,43 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(p->Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
+                .filter(p -> p.getName().contains("Kodilla"))
+                .forEach(trelloBoardDto -> {
+                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+                });
+    }
+//
+//
+//    @GetMapping("getTrelloBoards")
+//    public void getTrelloBoards() {
+//
+//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+//
+//        trelloBoards.forEach(trelloBoardDto -> {
+//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
+//        });
+//    }
+}

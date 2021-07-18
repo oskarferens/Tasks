843fb86e4c8899fd4a62686ec7f7b0e7254e057c
Oskar Ferens <oskar.ferens1@gmail.com>
2021-07-18 12:58:07 +0200

    22.4

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -2,12 +2,12 @@ package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 
 @RestController
 @RequestMapping("/v1/trello")
@@ -17,31 +17,29 @@ public class TrelloController {
     private final TrelloClient trelloClient;
 
     @GetMapping("getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        trelloBoards.stream()
-                .filter(p-> Objects.nonNull(p.getId()) && Objects.nonNull(p.getName()))
-                .filter(p -> p.getName().contains("Kodilla"))
-                .forEach(trelloBoardDto -> {
-                    System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName());
-                });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
-
-//    @GetMapping("getTrelloBoards")
-//    public void getTrelloBoards() {
-//        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-//        trelloBoards.forEach(trelloBoardDto -> {
-//            System.out.println(trelloBoardDto.getId() + " " + trelloBoardsDto.getName());
-//
-//            System.out.println("This board contains lists: ");
-//
-//            trelloBoardsDto.getLists().forEach(trelloList -> {
-//                System.out.println(trelloList.getName() + " - " + trelloList.getI() + " - " + trelloList.isClosed()
-//                );
-//            });
-//
-//        });
-//    }
+    @PostMapping("createTrelloCard")
+    public CreatedTrelloCard createdTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -1,26 +1,18 @@
 package com.crud.tasks.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class TrelloBoardDto {
+public class  TrelloCardDto {
 
-    @JsonProperty("id")
-    private String id;
-
-    @JsonProperty("name")
     private String name;
-
-    @JsonProperty("lists")
-    private List<TrelloListDto> lists;
-
+    private String description;
+    private String pos;
+    private String listId;
 }
